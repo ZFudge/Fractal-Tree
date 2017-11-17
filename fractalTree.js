5c932a21847792c26f0bbaaadfc094003b9004ba
zfudge <zacheryfudge@gmail.com>
2017-11-16 21:06:43 -0700

    initial recursion

--- fractalTree.js
+++ fractalTree.js
@@ -1,52 +1,38 @@
 var lines = {
+  seed: true,
   speed: 5,
   canvas: document.getElementById('canv'),
-  length: 50,
+  length: 100,
   width: 10,
   angle: 45,
   colors: ['#F0F','#FA0','#0F0','#0FF']
 };
-
 lines.context = lines.canvas.getContext('2d');
 lines.context.lineCap = 'round';
-lines.context.strokeStyle = lines.colors[Math.floor(Math.random()*lines.colors.length)];
-lines.context.lineWidth = lines.width;
-lines.context.beginPath();
-lines.context.moveTo(lines.canvas.width/2, lines.canvas.height);
-lines.context.lineTo(lines.canvas.width/2, lines.canvas.height - lines.length);
-lines.context.stroke();
+lines.context.fillStyle = 'black';
+lines.context.fillRect(0,0,lines.canvas.width,lines.canvas.height);
+lines.context.strokeStyle = 'white';
 
-function mainFunc(growths, x, y, len, w, a) {
-  if (growths > 0 && w > 0.5) {
-    
+function tree(growths, x, y, len, w, a) {
+  if (growths > 0) {
+    console.log(growths);
     lines.context.translate(x, y);
-    lines.context.rotate(a * Math.PI / 180);
-    
-    const destination =  - len;
-    const half = ( - len) / 2;
-    
-    lines.context.strokeStyle = lines.colors[Math.floor(Math.random()*lines.colors.length)];
-    lines.context.lineWidth = w;
+    (lines.seed) ? lines.seed = false : lines.context.rotate(a * Math.PI / 180);
     lines.context.beginPath();
     lines.context.moveTo(0, 0);
-    lines.context.lineTo(x, destination);//causes initial elongation
-    //lines.context.quadraticCurveTo(x-5, half, x, destination);//causes initial elongation
+    
+    const endLine = y - len;const half = (y - len) / 2;
+    lines.context.lineWidth = w;
+    lines.context.lineTo(0, -len);
     lines.context.stroke();
     
-    mainFunc(growths - 1, 0, destination, len * 0.8, w * 0.8, a);
-    lines.context.rotate(-a * Math.PI / 180);
-    lines.context.translate(x, y);
-    mainFunc(growths - 1, 0, destination, len * 0.8, w * 0.8, -a);
-    lines.context.rotate(a * Math.PI / 180);
-    lines.context.translate(x, y);
+    tree(growths - 1, 0, -len, len * 0.8, w * 0.8, a);
     
-    //lines.context.rotate(-a * Math.PI / 180);
-    //lines.context.translate(x, y);
-    //lines.context.translate(x, destination+len);
-    //mainFunc(growths - 1, 0, 0, len * 0.5, w * 0.6, -a);
   }
 }
-mainFunc(6, lines.canvas.width/2, lines.canvas.height - lines.length, lines.length * 0.8, lines.width * 0.8, lines.angle);
+
+tree(2, lines.canvas.width/2, lines.canvas.height, lines.length, lines.width, lines.angle);
 
 
-//let loop = setInterval(mainFunc,lines.speed);
+    //lines.context.strokeStyle = lines.colors[Math.floor(Math.random()*lines.colors.length)];
+//lines.context.quadraticCurveTo(x-5, half, x, destination);//causes initial elongation
