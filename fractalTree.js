02614bc2d971013e0f3c6b31e14cf32f0f4822fb
zfudge <zacheryfudge@gmail.com>
2017-11-18 16:09:11 -0700

    fixed curve

--- fractalTree.js
+++ fractalTree.js
@@ -4,47 +4,51 @@ const tree = {
   width: 10,
   angle: 45,
   branches: 4,
-  straight: true,
-  colors: ['#F0F','#FA0','#0F0','#0FF'],
+  curve: 0,
+  fruit: false,
+  colorful: true,
+  inverted: false,
+  colors: ['#3B0F00','#541500','#751D00','#8F2501',  '#2BAD00','#00DE1E','#6000DE','#AF02E8','#E80288','#E80253','#E8022C'],
+  //colors: ['#F0F','#FA0','#0F0','#0FF', '#0004FF','#8400FF'],
   colorStack: [],
   returnToDefault: false
 };
 const canvas = document.getElementById('canv');
 const context = canvas.getContext('2d');
 context.lineCap = 'round';
-//context.fillStyle = 'black';
-//context.fillRect(0,0,canvas.width,canvas.height);
 
-function grow(growths, x, y, len, w, a, c) {
+function grow(growths, x, y, len, w, a) {
   context.translate(x, y);
-  if (growths > 0) {
-    context.strokeStyle = tree.colorStack[growths-1];
+  if (growths < tree.branches) {
+    (tree.colorful) ? context.strokeStyle = tree.colors[growths] : (tree.inverted) ? context.strokeStyle = 'white' : context.strokeStyle = 'black';
     context.lineWidth = w;
     const endLine = -len, half = -len/2;
     if (!tree.seed) context.rotate(a * Math.PI / 180);
     context.beginPath();
     context.moveTo(0, 0);
-    const bend = Math.random() * 50;
-    //if (a < 0) bend -= bend * 2;
-    (tree.seed || tree.straight) ? context.lineTo(0, -len) : context.quadraticCurveTo(bend, half, x, -len); //causes initial elongation
+    const bend = Math.random();
+    //if (Math.random() < 0) bend -= bend * 4;
+    (tree.seed) ? context.lineTo(0, -len) : context.quadraticCurveTo(tree.curve * a * len / 50, half, x, -len); //causes initial elongation
     context.stroke();
     if (tree.seed) tree.seed = false;
 
     context.save();
     
-    const newColor = tree.colors[Math.floor(Math.random()*tree.colors.length)];
-    grow(growths - 1, 0, -len, len * 0.8, w * 0.8, a, newColor);
+    grow(growths + 1, 0, -len, len * 0.8, w * 0.8, a);
 
     context.restore();
     context.save();
     
-    grow(growths - 1, 0, -len, len * 0.8, w * 0.8, -a, newColor);
+    grow(growths + 1, 0, -len, len * 0.8, w * 0.8, -a);
     
     context.restore();
+  } else if (tree.fruit) {
+    context.fillStyle = tree.colors[Math.floor(Math.random() * tree.colors.length)];
+    context.beginPath();
+    context.arc(0,0, w * 2, 0,2 * Math.PI);
+    context.fill();
   }
 }
-//context.strokeStyle = tree.colors[Math.floor(Math.random()*tree.colors.length)];
-//context.quadraticCurveTo(x-5, half, x, destination);//causes initial elongation
 function adjustAngle(newAng) {
   tree.angle = parseInt(newAng);
   regrowth();
@@ -72,7 +76,7 @@ function resetWidth() {
 }
 
 function adjustLength(newLen) {
-  tree.length = parseInt(newLen);
+  tree.length = parseFloat(newLen);
   regrowth();
 }
 const lengthSlide = document.getElementById('lengthSlider');
@@ -97,12 +101,25 @@ function resetBranches() {
   regrowth();
 }
 
+function adjustCurve(newCurve) {
+  tree.curve = parseFloat(newCurve);
+  regrowth();
+}
+const curveSlide = document.getElementById('curveSlider');
+function resetCurve() {
+  if (tree.returnToDefault) {
+    tree.curve = 0;
+    curveSlide.value = 0;
+  }
+  regrowth();
+}
+
 function regrowth() {
   tree.seed = true;
-  context.fillStyle = 'white';
+  (tree.inverted) ? context.fillStyle = 'black' : context.fillStyle = 'white';
   context.fillRect(0,0,canvas.width,canvas.height);
   context.save();
-  grow(tree.branches, canvas.width/2, canvas.height*0.95, tree.length, tree.width, tree.angle, tree.colors[Math.floor(Math.random() * tree.colors.length)]); // , tree.colors[Math.floor(Math.random() * tree.colors.length)]
+  grow(0, canvas.width/2, canvas.height*0.95, tree.length, tree.width, tree.angle); //, tree.colors[Math.floor(Math.random() * tree.colors.length)]); // , tree.colors[Math.floor(Math.random() * tree.colors.length)]
   context.restore();
 }
 
@@ -117,9 +134,24 @@ function setColorStack() {
     tree.colorStack.push(tree.colors[Math.floor(Math.random() * tree.colors.length)]);
   }
 }
+function resetColor() {
+  setColorStack();
+  regrowth();
+}
 
+function colorOnOff() {
+  tree.colorful = !tree.colorful;
+  regrowth();
+}
+
+const invert = () => {
+  tree.inverted = !tree.inverted;
+  regrowth();
+};
+
+context.fillStyle = 'white';
+context.fillRect(0,0,canvas.width,canvas.height);
 setColorStack();
-
 context.save();
-grow(tree.branches, canvas.width/2, canvas.height*0.95, tree.length, tree.width, tree.angle, tree.colors[Math.floor(Math.random() * tree.colors.length)]); // , tree.colors[Math.floor(Math.random() * tree.colors.length)]
+grow(0, canvas.width/2, canvas.height*0.95, tree.length, tree.width, tree.angle, tree.colors[Math.floor(Math.random() * tree.colors.length)]); // , tree.colors[Math.floor(Math.random() * tree.colors.length)]
 context.restore();
